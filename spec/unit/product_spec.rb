12f50c8823efcf075b12c27fee7c474cd6995033
Road Tang <roadtang@gmail.com>
2013-11-02 17:50:43 +0800

    fix rspec test case bug

--- spec/unit/product_spec.rb
+++ spec/unit/product_spec.rb
@@ -4,15 +4,16 @@ describe Product do
 
   describe "attributes name and price" do
     it " should set attribute properly with intialize" do
-      p = Product.new(:name => "Skirt", :price => 23})
+      p = Product.new(:name => "Skirt", :price => 23.3)
       p.name.should eq("Skirt")
       p.price.should eq(23.3)
     end
 
     it "set & read attribute" do
+      p = Product.new(:name => "Skirt", :price => 23.3)
       p.name = 'Flower'
       p.price = 25.0
-      p.name.should.eq("Flower")
+      p.name.should eq("Flower")
       p.price.should eq(25)
     end
     
